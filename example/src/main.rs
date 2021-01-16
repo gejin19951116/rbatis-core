c941702c41317e102725905885f337405e518098
zhuxiujia <zhuxiujia@qq.com>
2021-01-16 11:30:22 +0800

    first add

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,13 +1,3 @@
-## rbatis-core driver
-
-rbatis 核心驱动程序
-
-#### support bigdecimal,json decode,async_std,tokio
-
-#### 支持serde_json解码,支持BigDecimal，支持序列化，反序列化，支持async_std和tokio,支持自动判断数据库类型(支持mysql，pg，sqlite，tidb等等相关驱动)
-
-```rust
-
 use rbatis_core::db::{DBPool};
 
 fn main() -> Result<(), rbatis_core::Error>{
@@ -22,5 +12,3 @@ fn main() -> Result<(), rbatis_core::Error>{
         }
     )
 }
-
-```
---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+//! Core of rbatis::core, the rust SQL toolkit. Not intended to be used directly.
+
+// #![warn(missing_docs)]
+#![allow(unused_imports)]
+#![allow(unused_assignments)]
+
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+extern crate serde_json;
+
+pub use error::{Error, Result};
+
+pub mod runtime;
+
+/// database
+#[cfg(feature = "mysql")]
+mod mysql;
+#[cfg(feature = "postgres")]
+mod postgres;
+#[cfg(feature = "sqlite")]
+mod sqlite;
+#[cfg(feature = "mssql")]
+mod mssql;
+#[macro_use]
+pub mod error;
+pub mod results;
+pub mod decode;
+pub mod db;
+pub mod convert;
+pub mod sync;
+pub mod value;
