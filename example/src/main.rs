e80d0be964e9af9074c863814faf05eab2f17199
zxj_pc <zhuxiujia@qq.com>
2021-03-12 21:42:54 +0800

    change to tokio runtime

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,12 +1,14 @@
 use rbatis_core::db::DBPool;
+use rbatis_core::Error;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     //Automatic judgment of database type
-    let pool = DBPool::new("mysql://root:123456@localhost:3306/test").await.unwrap();
-    let mut conn = pool.acquire().await.unwrap();
+    let pool = DBPool::new("mysql://root:123456@localhost:3306/test").await?;
+    let mut conn = pool.acquire().await?;
     let count: serde_json::Value = conn
         .fetch("SELECT count(1) FROM biz_activity;")
         .await.unwrap().0;
     println!("count: {}", count);
+    return Ok(());
 }
